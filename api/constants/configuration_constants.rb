a8ba1b15a010ae158f69f36ccc9d1f32d29ee8df
Grant Pidwell <grantpidwell@infinity-g.com>
2015-03-17 15:30:43 +0000

    Modified configuration_constants

--- api/constants/configuration_constants.rb
+++ api/constants/configuration_constants.rb
@@ -13,30 +13,30 @@ module ConfigurationConstants
         :logger_age => 10,
         :logger_size => 1024000,
         :default_request_timeout => 60,
-        :allowed_origin => 'http://localhost:8000, http://54.154.155.144:9002, http://ec2-54-154-155-144.eu-west-1.compute.amazonaws.com:9002',
+        :allowed_origin => 'http://localhost:8000',
         :sms_gateway_api_uri => 'https://rest.nexmo.com/sms/json',
         :sms_gateway_api_key => '03d116ba',
         :sms_gateway_api_secret => 'fb232c80',
         :sms_gateway_reply_number => '27877460911'
     }
 
-    TEST = {
-        :host => '0.0.0.0',
-        :port => 9004,
-        :api_auth_token => '7b2ebe64dc9149ac8a9e923bf2a6b233',
-        :mongo_host => 'localhost',
-        :mongo_port => 27017, # default is 27017
-        :mongo_db => 'ig-sms',
-        :logger_file => 'app_log.log',
-        :logger_age => 10,
-        :logger_size => 1024000,
-        :default_request_timeout => 60,
-        :allowed_origin => 'http://54.154.155.144:9002',
-        :sms_gateway_api_uri => 'https://rest.nexmo.com/sms/json',
-        :sms_gateway_api_key => '03d116ba',
-        :sms_gateway_api_secret => 'fb232c80',
-        :sms_gateway_reply_number => '27877460911'
-    }
+    # TEST = {
+    #     :host => '0.0.0.0',
+    #     :port => 9004,
+    #     :api_auth_token => '7b2ebe64dc9149ac8a9e923bf2a6b233',
+    #     :mongo_host => 'localhost',
+    #     :mongo_port => 27017, # default is 27017
+    #     :mongo_db => 'ig-sms',
+    #     :logger_file => 'app_log.log',
+    #     :logger_age => 10,
+    #     :logger_size => 1024000,
+    #     :default_request_timeout => 60,
+    #     :allowed_origin => 'http://54.154.155.144:9002',
+    #     :sms_gateway_api_uri => 'https://rest.nexmo.com/sms/json',
+    #     :sms_gateway_api_key => '03d116ba',
+    #     :sms_gateway_api_secret => 'fb232c80',
+    #     :sms_gateway_reply_number => '27877460911'
+    # }
 
     #
     # PRODUCTION = {
