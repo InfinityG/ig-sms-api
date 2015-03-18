9a7fea8a2233210d55440c0d8789c39abb9ee41d
Grant Pidwell <grantpidwell@infinity-g.com>
2015-03-18 09:05:13 +0000

    Modified access-control-allow-credentials in cors.rb

--- api/routes/cors.rb
+++ api/routes/cors.rb
@@ -11,7 +11,7 @@ module Sinatra
       app.options '*' do
         response.headers['Access-Control-Allow-Headers'] = 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Cache-Control, Accept, Authorization'
         response.headers['Access-Control-Allow-Origin'] = config[:allowed_origin]
-        response.headers['Access-Control-Allow-Credentials'] = 'true'
+        response.headers['Access-Control-Allow-Credentials'] = 'false'
         response.headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, OPTIONS'
       end
 
@@ -20,7 +20,7 @@ module Sinatra
           # Needed for AngularJS
           response.headers['Access-Control-Allow-Headers'] = 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Cache-Control, Accept, Authorization, Content-Length'
           response.headers['Access-Control-Allow-Origin'] = config[:allowed_origin]
-          response.headers['Access-Control-Allow-Credentials'] = 'true'
+          response.headers['Access-Control-Allow-Credentials'] = 'false'
           response.headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, OPTIONS'
 
           # halt 200
