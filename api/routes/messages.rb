a8c024eda37ea8ab58a644b9f11e8e62c0e7a04f
Grant Pidwell <grantpidwell@infinity-g.com>
2015-03-19 13:18:24 +0000

    Modified inbound message route

--- api/routes/messages.rb
+++ api/routes/messages.rb
@@ -48,17 +48,36 @@ module Sinatra
       # &keyword=YK2BA
       # &message-timestamp=2015-03-18+10%3A59%3A45
 
-      app.get '/messages/inbound' do
+      # app.get '/messages/inbound' do
+      #
+      #   content_type :json
+      #
+      #   begin
+      #     sender_number = params[:msisdn]
+      #     message_id = params[:messageId]
+      #     short_hash = params[:keyword]
+      #     text = params[:text]
+      #
+      #     result = MessageService.new.update_inbound_message short_hash, text, sender_number, message_id
+      #     puts result
+      #
+      #     status 200
+      #   rescue SmsError => e
+      #     status 200 # this should be 500 but the SMS provider needs a 200
+      #     puts e.message.to_json
+      #   end
+      #
+      # end
 
-        content_type :json
+      app.post '/messages/inbound' do
+
+        data = JSON.parse(request.body.read, :symbolize_names => true)
 
         begin
-          sender_number = params[:msisdn]
-          message_id = params[:messageId]
-          short_hash = params[:keyword]
-          text = params[:text]
-
-          puts "Sender number: #{sender_number}; message_id: #{message_id}; short_hash:#{short_hash}; text: #{text}"
+          sender_number = data[:msisdn]
+          message_id = data[:messageId]
+          short_hash = data[:keyword]
+          text = data[:text]
 
           result = MessageService.new.update_inbound_message short_hash, text, sender_number, message_id
           puts result
